0e8907d3437b69d55595d9f0488c02f5ec1231d4
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-10-19 17:09:50 -0700

    Add_session_controller_and_helpers

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,23 @@
+class SessionController < ApplicationController
+  
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:sessions][:email])
+    if @user && @user.authenticate(params[:sessions][:password])
+       log_in @user
+       redirect_to user_url(@user)
+    else
+      # flash[:danger] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,43 +6,10 @@ Rails.application.routes.draw do
   resources :users, except: [:index]
   resources :shift
   resources :expense
-  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
-  # get 'expense/index'
-
-  # get 'expense/show'
-
-  # get 'expense/new'
-
-  # get 'expense/create'
-
-  # get 'expense/update'
-
-  # get 'expense/destroy'
-
-  # get 'shift/index'
-
-  # get 'shift/show'
-
-  # get 'shift/new'
-
-  # get 'shift/create'
-
-  # get 'shift/update'
-
-  # get 'shift/destroy'
-
-  # get 'user/show'
-
-  # get 'user/index'
-
-  # get 'user/new'
-
-  # get 'user/create'
-
-  # get 'user/destroy'
-
-  # get 'user/update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
