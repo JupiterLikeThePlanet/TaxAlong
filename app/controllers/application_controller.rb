85f823b82a6e084e8bdffedd9ebc069ffac0a6e0
Jupiter <jupiterbaudot@gmail.com>
2017-02-07 15:58:23 -0800

    Refactor Session Controller name to Sessions and create new view page for sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # include Pundit
-  include SessionHelper
+  include SessionsHelper
 end
