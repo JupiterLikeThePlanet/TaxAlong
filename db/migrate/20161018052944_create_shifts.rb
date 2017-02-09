34568c0fe7aaa515687a6678a6739d56b212b9a7
Jupiter <jupiterbaudot@gmail.com>
2017-02-08 22:21:57 -0800

    Change end mileage in shift model to integer

--- db/migrate/20161018052944_create_shifts.rb
+++ db/migrate/20161018052944_create_shifts.rb
@@ -2,7 +2,7 @@ class CreateShifts < ActiveRecord::Migration[5.0]
   def change
     create_table :shifts do |t|
       t.integer :start_mileage
-      t.string :end_mileage
+      t.integer :end_mileage
       t.datetime :date
       t.decimal :earnings
       t.references :user
