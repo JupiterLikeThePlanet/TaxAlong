8103ab3c51485dcb33e36b0b9975a8c926c3c66c
Jupiter <jupiterbaudot@gmail.com>
2017-02-09 19:47:22 -0800

    Render first component, add react rails to gem file

---
+++ app/assets/javascripts/components/adjust_income.js.jsx
@@ -0,0 +1,20 @@
+// var React = require("react-rails");
+
+var AdjustIncome = React.createClass({
+  // propTypes: {
+  //   startMileage: React.PropTypes.node,
+  //   endMileage: React.PropTypes.node,
+  //   gas: React.PropTypes.node
+  // },
+
+  render: function() {
+    return (
+      <div>
+        <h1>RENDERED COMPONENT</h1>
+      </div>
+    );
+  }
+});
+
+
+// module.exports = AdjustIncome;
