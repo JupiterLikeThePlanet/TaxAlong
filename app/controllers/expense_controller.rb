b6c62ad72bccac51614df06a4316eb06120be5ac
Jupiter <jupiterbaudot@gmail.com>
2017-02-15 23:01:33 -0800

    .idea/workspace.xml

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -7,9 +7,18 @@ class ExpenseController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:id])
+    @expense = @user.expense.new
   end
 
   def create
+    @expense = current_user.expenses.new(expense_params)
+
+    if @expense.save!
+      redirect_to expense_path(@expense.id)
+    else
+      render :new
+    end
   end
 
   def update
@@ -17,4 +26,11 @@ class ExpenseController < ApplicationController
 
   def destroy
   end
+end
+
+
+private
+
+def expense_params
+  params.require(:expenses).permit(:type, :cost)
 end
